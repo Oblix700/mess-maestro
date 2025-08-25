5fae1f231ac5f813a87f2cbdaf1d48d2caa942ee
Francois Marsau <francois.marsau@gmail.com>
2025-08-25 16:05:23 +0000

    no change

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default function DashboardLayout({
         </Sidebar>
         <div className="flex flex-col sm:pl-14 peer-[[data-state=expanded]]:sm:pl-64 transition-[padding-left] ease-in-out duration-200">
           <Header />
-          <main className="flex-1">
+          <main className="p-4 sm:px-6">
             {children}
           </main>
         </div>
