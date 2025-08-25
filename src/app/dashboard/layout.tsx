19e66832f329e8ba1488b4a7e0c3b9ea132af569
Francois Marsau <francois.marsau@gmail.com>
2025-08-25 16:10:20 +0000

    i cant believe we cant fix this simple thing. do a deep dive into code f

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,13 +15,13 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <div className="min-h-screen w-full bg-muted/40">
         <Sidebar>
           <SidebarNav />
         </Sidebar>
         <div className="flex flex-col sm:pl-14 peer-[[data-state=expanded]]:sm:pl-64 transition-[padding-left] ease-in-out duration-200">
           <Header />
-          <main className="px-4 sm:px-6">
+          <main className="p-4 sm:px-6">
             <div className="h-full overflow-auto">
               {children}
             </div>
