2fc112152de8dc798498017d1bc359bff278354c
Francois Marsau <francois.marsau@gmail.com>
2025-08-28 09:18:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -18,7 +18,10 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    devRevalidate: false,
+    // This is required to allow the Firebase Studio development environment to communicate with the Next.js server.
+    allowedDevOrigins: [
+        "https://6000-firebase-studio-1756054617899.cluster-fbfjltn375c6wqxlhoehbz44sk.cloudworkstations.dev",
+    ],
   },
 };
 
